6427b5cbb8dac2c55bf9d63afe5933eae833e4bc
hiwot nega <hiwotmanaye2@gmail.com>
2018-10-30 13:03:00 -0700

    book initial

--- server.js
+++ server.js
@@ -1,10 +1,43 @@
 const express = require('express');
 const app = express();
-app.get('/',function(req,res){
-  
-  res.send('hello');
+const superagent = require('superagent');
+require('dotenv').config()
+
+const PORT = process.env.PORT||3000
+
+// app.set('views', './views/pages');
+app.set('view engine', 'ejs');
+app.get('/',newSearch)
+
+  // res.render('pages/index');
+
+ 
+app.post('/searches',createSearch);
+// app.use(express.static(__dirname + '/views/pages'));
+app.listen(PORT,function(){
+    console.log(`server running at ${PORT}`);
 });
-let port = 3000;
-app.listen(port,function(){
-    console.log(`server running at ${port}`);
-});
+
+// function Book(info){
+//   const placeholder Image = 
+// }
+
+function newSearch (req,res){
+  res.render('pages/index');
+}
+function createSearch(req,res){
+   let URL = 'https://www.googleapis.com/books/v1/volumes?q='
+
+   if (request.body.search[1]==='title'){Url += `+intitle:${req.body.serach[0]}`;}
+   if (request.body.search[1]==='title'){Url += `+inauthor:${request.body.serach[0]}`;}
+   
+   
+   superagent .get (url)
+   .then(apiResponse=>apiResponse.body.item.map(bookResalt=> new Book (bookResalt.volumeinfo)))
+   .then(results=>{
+   response.render('pages/show',{items:results})
+
+})
+}
+ 
+
