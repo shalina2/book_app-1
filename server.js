117d116c0a7f863b42a59be0eea3ba700c288e56
hiwot nega <hiwotmanaye2@gmail.com>
2018-10-30 08:10:12 -0700

    test local app

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+app.get('/',function(req,res){
+  
+  res.send('hello');
+});
+let port = 3000;
+app.listen(port,function(){
+    console.log(`server running at ${port}`);
+})
+app.lis
